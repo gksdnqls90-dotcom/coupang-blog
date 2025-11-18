92426aea59d02a92237d705d9acbef29d4f15de4
gksdnqls90-dotcom <gksdnqls90@gmail.com>
2025-11-18 15:51:22 +0900

    newicbh대공사

---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDY7gAi-56LPPvPKRNNhok6CnY2AcoaUVc",
+    authDomain: "icbhplus.firebaseapp.com",
+    projectId: "icbhplus",
+    storageBucket: "icbhplus.firebasestorage.app",
+    messagingSenderId: "141219147304",
+    appId: "1:141219147304:web:11b6004ce940b300af4f1d",
+    measurementId: "G-67TTP05JJZ"
+};
